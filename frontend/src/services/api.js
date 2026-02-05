6d8ed31f5e58d4cf4072b7097eaa1ea990d56060
MD Mutasim Billah Saad <mdmutasimbillahsaad@gmail.com>
2026-02-05 11:49:36 +0600

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -17,8 +17,8 @@ api.interceptors.request.use((config) => {
 });
 
 export const authService = {
-  register: (name, email, mobile, password, role) =>
-    api.post('/auth/register', { name, email, mobile, password, role }), // ✅ baseURL + endpoint
+register: (name, email, mobile, password, role) =>
+  axios.post('https://staynest-backend-n2kn.onrender.com/api/v1/auth/register', { name, email, mobile, password, role });
   login: (email, password) =>
     api.post('/auth/login', { email, password }),
   getCurrentUser: () => api.get('/auth/me'),
